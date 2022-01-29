fb19e431b6b42e78cf31e9cb60b1d3832f8ac8be
Alicja <alichr07@gmail.com>
2022-01-29 04:14:04 +0100

    1_WprowadzenieBezLINQ

--- 1 Wprowadzenie/Program.cs
+++ 1 Wprowadzenie/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace _1_Wprowadzenie
 {
@@ -6,7 +8,29 @@ namespace _1_Wprowadzenie
     {
         static void Main(string[] args)
         {
-            
+            var sciezka = @"c:\windows";
+            PokazDuzePlikiBezLinq(sciezka);
+        }
+
+        private static void PokazDuzePlikiBezLinq(string sciezka)
+        {
+            DirectoryInfo katalog = new DirectoryInfo(sciezka);
+            FileInfo[] pliki = katalog.GetFiles();
+
+            Array.Sort(pliki, new FileInfoComparer());
+
+            for (var i = 0; i < 5; i++)
+            {
+                FileInfo plik = pliki[i];
+                Console.WriteLine($"{plik.Name, -20} : {plik.Length, 20:N0}");
+            }
+        }
+    }
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
