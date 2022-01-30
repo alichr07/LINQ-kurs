28cd14b71a5ebb0d92986996b64558b39eebdec1
Alicja <alichr07@gmail.com>
2022-01-30 02:47:26 +0100

    IEnumerableLINQ

---
+++ 2_Funkcje/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2_Funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Pracownik> programisci = new Pracownik[]
+            {
+                new Pracownik { Id = 1, Imie = "Marcin", Nazwisko = "Nowak" },
+                new Pracownik { Id = 2, Imie = "Tomek", Nazwisko = "Kowal" },
+                new Pracownik { Id = 3, Imie = "Jacek", Nazwisko = "Sobota" },
+                new Pracownik { Id = 4, Imie = "Adam", Nazwisko = "Wrona" },
+            };
+
+            IEnumerable<Pracownik> kierowcy = new List<Pracownik>()
+            {
+                new Pracownik { Id = 5, Imie = "Olek", Nazwisko = "Sroka" },
+                new Pracownik { Id = 6, Imie = "Piotr", Nazwisko = "Wróbel" },
+                new Pracownik { Id = 7, Imie = "Marek", Nazwisko = "Uszak" },
+            };
+
+            IEnumerator<Pracownik> enumerator = programisci.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Imie);
+            }
+        }
+    }
+}
