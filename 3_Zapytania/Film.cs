8ded8c90807bd313414485d3e821f98919c9f870
Alicja <alichr07@gmail.com>
2022-02-04 02:40:32 +0100

    WlasnyFiltr

---
+++ 3_Zapytania/Film.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Zapytania
+{
+    class Film
+    {
+        public string Tytul { get; set;}
+        public string Gatunek { get; set; }
+        public float Ocena { get; set; }
+        public int Rok { get; set; }
+    }
+}
