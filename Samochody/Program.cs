1faab707fea0444de1166878bbc5208b1dbf64b4
Alicja <alichr07@gmail.com>
2022-02-19 18:31:24 +0100

    LinqIEntityFramework

--- Samochody/Program.cs
+++ Samochody/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Xml.Linq;
 
 namespace Samochody
@@ -10,14 +12,52 @@ namespace Samochody
     {
         static void Main(string[] args)
         {
-            TworzenieXML();
-            ZapytanieXML();
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SamochodDB>());
+            WstawDane();
+            ZapytanieDane();
+        }
+
+        private static void WstawDane()
+        {
+            var samochody = WczytywanieSamochodu("paliwo.csv");
+            var db = new SamochodDB();
+            db.Database.Log = Console.WriteLine;
+
+            if (!db.Samochody.Any())
+            {
+                foreach (var samochod in samochody)
+                {
+                    db.Samochody.Add(samochod);
+                }
+                db.SaveChanges();
+            }
+        }
+
+        private static void ZapytanieDane()
+        {
+            var db = new SamochodDB();
+            db.Database.Log = Console.WriteLine;
+
+            var zapytanie = from samochod in db.Samochody
+                            orderby samochod.SpalanieAutostrada descending, samochod.Model ascending
+                            select samochod;
+
+            var zapytanie2 = db.Samochody.Where(s => s.Model == "Audi")
+                                         .OrderByDescending(s => s.SpalanieAutostrada).ThenBy(s => s.Model).Take(10);
+
+            foreach (var samochod in zapytanie2)
+            {
+                Console.WriteLine($"{samochod.Model} : {samochod.SpalanieAutostrada}");
+            }
         }
 
         private static void ZapytanieXML()
         {
+            XNamespace ns = "namespace";
+            XNamespace ex = "namespace/ex";
+
             var dokument = XDocument.Load("paliwo.xml");
-            var zapytanie = from element in dokument.Element("Samochody").Elements("Samochod")
+            var zapytanie = from element in dokument.Element(ns + "Samochody")?.Elements(ex + "Samochod") ?? Enumerable.Empty<XElement>()
                             where element.Attribute("Producent2")?.Value == "Ferrari"
                             select new
                             {
@@ -33,17 +73,21 @@ namespace Samochody
 
         private static void TworzenieXML()
         {
+            XNamespace ns = "namespace";
+            XNamespace ex = "namespace/ex";
+
             var rekordy = WczytywanieSamochodu("paliwo.csv");
 
             var dokument = new XDocument();
-            var samochody = new XElement("Samochody", from rekord in rekordy
-                                                      select new XElement("Samochod",
+            var samochody = new XElement(ns + "Samochody", from rekord in rekordy
+                                                      select new XElement(ex + "Samochod",
                                                                             new XAttribute("Rok", rekord.Rok),
                                                                             new XAttribute("Producent", rekord.Producent),
                                                                             new XAttribute("Model", rekord.Model),
                                                                             new XAttribute("SpalanieAutostrada", rekord.SpalanieAutostrada),
                                                                             new XAttribute("SpalanieMiasto", rekord.SpalanieMiasto),
                                                                             new XAttribute("SpalanieMieszane", rekord.SpalanieMieszane)));
+            samochody.Add(new XAttribute(XNamespace.Xmlns + "ex", ex));
 
             dokument.Add(samochody);
             dokument.Save("paliwo.xml");
